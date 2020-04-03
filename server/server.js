1cb7bc28dd038bf6138f2b0d6d23241e8cde812d
francesco <zuppif@usi.ch>
2020-04-03 20:50:48 +0200

    gitflow-feature-stash: test

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require('apollo-server-express')
+const typeDefs = require('./types/index.js')
+const resolvers = require('./resolvers/index.js')
+const { AuthDirective } = require('./directives/index.js')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    schemaDirectives: {
+        isAuthenticated: AuthDirective
+    },
+    context: ({ req }) => ({
+        // we store the request on the context in order to use them later inside the directives
+        req
+    })
+})
+
+module.exports = server
---
+++ server/test/test.js
@@ -0,0 +1,79 @@
+const { gql } = require('apollo-server-express')
+const { createTestClient } = require("apollo-server-testing")
+const server = require('../server')
+const { expect, assert } = require("chai")
+const mongoose = require('mongoose')
+const { query, mutate } = createTestClient(server)
+
+const MONGO_URI = 'mongodb://localhost/magic-test'
+
+
+let connection
+const user = { username: 'test', email: 'test', password: 'test ' }
+
+const NEW_USER = gql`
+    mutation newUser($input: UserInput!){
+        newUser(input: $input){
+            username
+            email
+        }
+    }
+`
+
+const NEW_AUTH = gql`
+    mutation newAuth($email: String!, $password: String!) {
+        newAuth(email: $email, password: $password){
+            token 
+            user {
+                email
+            }
+        }
+    }
+`
+
+
+
+describe("API", () => {
+    before(async () => {
+        connection = await mongoose.connect(MONGO_URI)
+        console.log(`🚀 Mongodb connected at ${MONGO_URI}`)
+    })
+    describe('Auth', () => {
+        it('Should create a new user', async () => {
+            const res = await mutate({
+                mutation: NEW_USER, 
+                variables: { input: user }
+            })
+            const newUser = res.data.newUser
+            assert.equal(newUser.email, user.email)
+            assert.equal(newUser.username, user.username)
+
+        })
+        it('Should return error if user already exist', async () => {
+            const res = await mutate({
+                mutation: NEW_USER, 
+                variables: { input: user }
+            })
+        
+           assert.equal(res.errors[0].message, 'Email already used.')
+        })
+
+        it('Should return a new JWT', async () => {
+            const res = await mutate({
+                mutation: NEW_AUTH, 
+                variables: { email: user.email, password: user.password }
+            })  
+
+            assert.exists(res.data.newAuth.token)
+            assert.isString(res.data.newAuth.token)
+            assert.equal(res.data.newAuth.user.email, user.email)
+
+        //    assert.equal(res.errors[0].message, 'Email already used.')
+        })
+        
+    })
+
+    after(async () => {
+        await connection.connection.db.dropDatabase()
+    })
+})
