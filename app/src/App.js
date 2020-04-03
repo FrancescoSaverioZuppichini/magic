22da5d97859e7f7f3d01d9a27064bd78f394e4dc
francesco <zuppif@usi.ch>
2020-04-03 21:22:17 +0200

    basic routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import theme from './theme.js'
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: '/graphql',
 })
 
-
 function App() {
+  const [isAuthenticated, _] = useState(false)
+
   return (
     <ApolloProvider client={client}>
-        <BrowserRouter>
-          <Route path="/" exact={true}>
-            index
+      <BrowserRouter>
+        <Route path="/" exact={true}>
+          index
           </Route>
-          <Route path="/home">
-            home
+        <Route path="/home">
+          home
           </Route>
-        </BrowserRouter>
+        {isAuthenticated && <Redirect to={{ pathname: '/home' }} />}
+      </BrowserRouter>
     </ApolloProvider>
   )
 }
