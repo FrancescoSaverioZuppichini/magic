35cdce3d750215feca5ba856eb5b84f321305189
francesco <zuppif@usi.ch>
2020-04-04 10:22:04 +0200

    index page

--- app/src/Index.js
+++ app/src/Index.js
@@ -1,13 +1,41 @@
 import React from 'react'
-import { Flex, Text, Button, Card } from 'theme-ui'
+import { Flex, Box, Input, Text, Button, Card } from 'theme-ui'
 
 function Index() {
     return (
-        <Flex>
-            <Card variant='viewport' sx={{ flex: 1,  backgroundColor: 'white' }}>
-                Index
+        <Flex sx={{ flexDirection: ['column', 'column', 'row'], height: '100%' }}>
+            <Card variant='viewport' sx={{
+                flex: 1,
+                backgroundColor: 'white',
+            }}>
+                  <Flex sx={{ flexDirection: 'column', height: '100%' }}>
+                    <Box variant='spacer'/>
+                    <Box px={[2,2]} sx={{maxWidth: '400px', width: '80%', alignSelf: 'center'}}>
+                        <Input mb={3}></Input>
+                        <Input mb={3}></Input>
+                        <Input ></Input>
+                    </Box>
+                    <Box variant='spacer' />
+                </Flex>
             </Card>
-            <Card variant='viewport' sx={{ flex: 1, backgroundColor: 'background' }}></Card>
+            <Card variant='viewport' sx={{ flex: 1, backgroundColor: 'background' }}>
+                <Flex sx={{ flexDirection: 'column', height: '100%' }}>
+                    <Box variant='spacer' />
+                    <Box>
+                        <Text sx={{ fontSize: 5, textAlign: 'center' }}>Play Magic <strong>effortless</strong></Text>
+                        <Flex py={[2, 2, 4]} sx={{ alignItems: 'center' }}>
+                            <Box variant='spacer' />
+                            <Text px={2} sx={{ fontSize: 4 }}>just</Text>
+                            <Button variant='primary'><Text>Sing Up</Text></Button>
+                            <Text px={2} sx={{ fontSize: 4 }}>or</Text>
+                            <Button variant='outline'><Text>Sing In</Text></Button>
+                            <Box variant='spacer' />
+                        </Flex>
+
+                    </Box>
+                    <Box variant='spacer' />
+                </Flex>
+            </Card>
         </Flex>
     )
 }
--- app/src/theme.js
+++ app/src/theme.js
@@ -1,34 +1,88 @@
 const theme = {
-    colors: {
-      text: '#000',
-      background: '#F4F4F4',
-      primary: '#6A66F2',
+  colors: {
+    text: '#000',
+    background: '#F4F4F4',
+    primary: '#6A66F2',
+    active: '#9B66F2'
+  },
+  fonts: {
+    body: "'Roboto', sans-serif",
+    heading: "'Roboto', sans-serif",
+    monospace: 'Menlo, monospace',
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  fontSizes: [
+    14, 16, 20, 24, 28, 48, 64,
+  ],
+  app: {
+    margin: 0,
+    width: '100vw',
+    height: '100vh'
+  },
+  cards: {
+    viewport: {
+      height: ['50vh', '50vh', '100vh'],
+      padding: [2, 4]
+    }
+  },
+  spacer: {
+    flex: '1 1'
+  },
+  forms: {
+    input: {
+      borderColor: 'primary',
+      borderRadius: '20px',
+      borderWidth: '3px',
+      outline:'none',
+      '&:active': {
+        // backgroundColor: 'active',
+        borderColor: 'active',
+        outline:'none'
+      },
+      '&:focus': {
+        // backgroundColor: 'active',
+        borderColor: 'active',
+        outline:'none'
+      }
     },
-    fonts: {
-        body: "'Roboto', sans-serif",
-        heading: "'Roboto', sans-serif",
-        monospace: 'Menlo, monospace',
+  },
+  buttons: {
+    primary: {
+      borderRadius: '20px',
+      borderStyle: 'solid',
+      borderColor: 'primary',
+      borderWidth: '3px',
+      outline: 'none',
+      '&:active': {
+        backgroundColor: 'active',
+        borderColor: 'active',
+        // outline:'none'
+      }
+
     },
-    fontWeights: {
-      body: 400,
-      heading: 700,
-      bold: 700,
-    },
-    lineHeights: {
-      body: 1.5,
-      heading: 1.125,
-    },
-    app: {
-        margin: 0,
-        width: '100vw',
-        height: '100vh'
-    },
-    cards: {
-        viewport: {
-            height: '100vh',
-            padding:[2,4]
-        }
+    outline: {
+      borderRadius: '20px',
+      backgroundColor: 'rgba(0,0,0,0)',
+      borderColor: 'primary',
+      color: 'primary',
+      borderStyle: 'solid',
+      borderWidth: '3px',
+      outline: 'none',
+      '&:active': {
+        borderColor: 'active',
+        color: 'active',
+        // outline:'none'
+      }
     }
   }
+}
 
 export default theme
