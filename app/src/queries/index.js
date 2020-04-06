59409501eb60d351d48748a7c5c51fc987f5256a
francesco <zuppif@usi.ch>
2020-04-06 19:05:05 +0200

    search for cards working

--- app/src/queries/index.js
+++ app/src/queries/index.js
@@ -11,7 +11,22 @@ const queries = {
     IS_AUTHENTICATED: gql`
         query isAuthenticated {
           isAuthenticated @client
-    }`
+    }`,
+    GET_CARDS: gql`
+        query cards($filter: CardFilter!, $cursor: CursorInput!){
+            cards(filter: $filter, cursor: $cursor) @connection(key: "cards"){
+                cards {
+                    id
+                    name
+                    scryfallId
+                },
+                hasMore
+                cursor {
+                    limit
+                    skip
+                }
+            }
+        }`
 }
 
 export default queries
