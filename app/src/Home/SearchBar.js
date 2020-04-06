a298e685e56a37ce66e14c871a837a04b74f47e9
francesco <zuppif@usi.ch>
2020-04-06 19:05:05 +0200

    search for cards working

--- app/src/Home/SearchBar.js
+++ app/src/Home/SearchBar.js
@@ -1,32 +1,14 @@
 import React, { useRef } from 'react'
 import { Input, IconButton, Box } from 'theme-ui'
-import { useHistory, useLocation } from 'react-router-dom'
-import queryString from 'query-string'
 
-function SearchBar() {
+function SearchBar({ onSearchClick}) {
     const input = useRef(null)
-    const history = useHistory()
-    const location = useLocation()
 
-    let { filter } = queryString.parse(location.search)
-    // restore old search value!
-    if (filter) input.current.value = filter
-
-    const searchCards = () => {
-        const filter = JSON.parse(input.current.value)
-        // call guery
-        history.push(
-            {
-                pathname: '/home',
-                search: `?${queryString.stringify({ 'filter': input.current.value })}`
-            }
-        )
-    }
 
     return (
         <Box sx={{ position: 'relative' }}>
             <Input variant='searchbar' placeholder='{ "name" : "search me", "type": "creature" }' ref={input}></Input>
-            <IconButton onClick={searchCards}
+            <IconButton onClick={() => onSearchClick(input.current.value)}
                 sx={{
                     position: 'absolute', top: 0, right: 0, bg: 'dark', height: '100%'
                     , borderRadius: '0px 16px 16px 0px', outline: 'none',
