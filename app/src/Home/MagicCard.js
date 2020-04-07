0cf7463175eabaf231cadd17d8ae1712bdb53db7
francesco <zuppif@usi.ch>
2020-04-07 16:47:46 +0200

    card search works

--- app/src/Home/MagicCard.js
+++ app/src/Home/MagicCard.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import {Flex, Card } from 'theme-ui'
+import React, { useState } from 'react'
+import { Box, Flex, Card, IconButton } from 'theme-ui'
+
+// TODO would be better to go to the next if clicked
+const MagicCardZoom = ({ scryfallId, onClose }) => (
+    <Flex sx={{
+        position: 'absolute',
+        top: 0, left: 0,
+        zIndex: 99, width: '100vw', height: '100vh', flexDirection: 'row', bg: 'rgba(244, 244, 244, 0.8)'
+    }}>
+        <Box variant="spacer" onClick={onClose} />
+        <Box sx={{ width: ['100vw', '50%'] }}>
+            <Card>
+                <IconButton onClick={onClose}>
+                    <img height='24px' src='/close-black-18dp.svg'></img>
+                </IconButton>
+                <img src={`cards/${scryfallId}.jpg`} width='100%'></img>
+            </Card>
+        </Box>
+        <Box variant="spacer" onClick={onClose} />
+    </Flex>
+)
 
 export default function MagicCard({ name, scryfallId }) {
+    const [isZooming, setIsZooming] = useState(false)
+
     return (
-        <Card p={2}>
-            <img src={`cards/${scryfallId}.jpg`} height='250px'></img>
+        <Card p={2} sx={{ flex: '1', flexBasis: '200px' }}>
+            <img src={`cards/${scryfallId}.jpg`} width='200px'
+                onClick={() => setIsZooming(true)}></img>
+            {isZooming && <MagicCardZoom
+                scryfallId={scryfallId}
+                onClose={() => setIsZooming(false)}
+            />}
         </Card>
     )
 }
--- app/src/Home/NavBar.js
+++ app/src/Home/NavBar.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Flex, Box, Text, Button } from 'theme-ui'
+import { Flex, Box, Text, Button, Card } from 'theme-ui'
 import SearchBar from './SearchBar'
-import Cards from './MagicCards'
+import MagicCards from './MagicCards'
 import { useLazyQuery } from '@apollo/react-hooks';
 import queries from '../queries/index'
 import { useHistory, useLocation } from 'react-router-dom'
 import queryString from 'query-string'
 
 
-const MagicCardsDisplayer = ( { cards, onLoadMore} ) => (
-    <div></div>
+const WithMagicCardsDisplayer = ({ children }) => (
+    <Card sx={{ position: 'absolute', left: 0, top: 6, bg: 'background' }}>
+        <Box px={[2, 3]}>
+            {children}
+        </Box>>
+    </Card>
 )
 
 function NavBar({ user }) {
@@ -19,11 +23,18 @@ function NavBar({ user }) {
             <Flex py={3} px={4} sx={{ width: '100vw', bg: 'primary', color: 'textLight', alignItems: 'center' }}>
                 <Box variant="spacer" />
                 <Box sx={{ flexGrow: 2 }}>
-                    <SearchBar children={({ cards, onLoadMore}) => (<Box>
-                            {cards && <Cards cards={cards.cards} />}
-                            {cards && cards.hasMore && <Button onClick={onLoadMore}>More</Button>}
-                        </Box>
-                        )}>
+                    <SearchBar children={({ cards, onLoadMore }) => (<Box>
+                        {cards && <WithMagicCardsDisplayer>
+                            <MagicCards cards={cards.cards} />
+                            {cards && cards.hasMore &&
+                                <Flex sx={{ width: '100%' }}>
+                                    <Box variant='spacer' />
+                                    <Button onClick={onLoadMore}>More</Button>
+                                    <Box variant='spacer' />
+                                </Flex>}
+                        </WithMagicCardsDisplayer>}
+                    </Box>
+                    )}>
                     </SearchBar>
                 </Box>
                 <Box variant="spacer" />
