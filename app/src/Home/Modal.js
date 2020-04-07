f54b82506b12bc567ce8ef714a65227ddc8714a7
francesco <zuppif@usi.ch>
2020-04-07 20:49:50 +0200

    modal components for newDeck and newRoom

--- app/src/Home/Modal.js
+++ app/src/Home/Modal.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { Box, Flex, Card } from 'theme-ui'
 
 export default function Modal(props) {
-    console.log(props)
     return (<div>
         {props.active && (<Flex p={2} sx={{
             position: 'fixed',
             top: 0, left: 0,
-            zIndex: 99, width: '100vw', height: '100vh', 
+            zIndex: 99, width: '100vw', height: '100vh',
             bg: 'rgba(244, 244, 244, 0.8)'
         }} variant='centering'>
             {props.children}
