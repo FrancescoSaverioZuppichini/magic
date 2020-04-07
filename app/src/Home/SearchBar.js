e52387ef61bfb26ecca230993ec28876f3d30761
francesco <zuppif@usi.ch>
2020-04-07 15:14:01 +0200

    minor changes

--- app/src/Home/SearchBar.js
+++ app/src/Home/SearchBar.js
@@ -1,14 +1,51 @@
 import React, { useRef } from 'react'
-import { Input, IconButton, Box } from 'theme-ui'
+import { Input, IconButton, Box, Button } from 'theme-ui'
+import Cards from './MagicCards'
+import { useLazyQuery } from '@apollo/react-hooks';
+import queries from '../queries/index'
+import { useHistory, useLocation } from 'react-router-dom'
 
-function SearchBar({ onSearchClick}) {
+import queryString from 'query-string'
+function SearchBar({children}) {
     const input = useRef(null)
+    const [getCards, { error, data, fetchMore }] = useLazyQuery(queries.GET_CARDS, 
+        { fetchPolicy: 'network-only' })
+    const history = useHistory()
+    
+    const searchCards = (value) => {
+        const filter = JSON.parse(value)
+        let cursor = { skip: 0, limit: 32 }
+
+        getCards({ variables: { filter, cursor } })
+
+        history.push(
+            {
+                pathname: '/home',
+                search: `?${queryString.stringify({ 'filter': value })}`
+            }
+        )
+    }
+
+    const onLoadMore = () => {
+        fetchMore({
+            variables: {
+                cursor: { limit: data.cards.cursor.limit, skip: data.cards.cursor.skip }
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+                if (!fetchMoreResult) return prev
+                fetchMoreResult.cards.cards = [...prev.cards.cards, ...fetchMoreResult.cards.cards]
+                return Object.assign({}, prev, fetchMoreResult)
+
+            }
+        })
+    }
 
 
     return (
+        <Box>
         <Box sx={{ position: 'relative' }}>
             <Input variant='searchbar' placeholder='{ "name" : "search me", "type": "creature" }' ref={input}></Input>
-            <IconButton onClick={() => onSearchClick(input.current.value)}
+            <IconButton onClick={() => searchCards(input.current.value)}
                 sx={{
                     position: 'absolute', top: 0, right: 0, bg: 'dark', height: '100%'
                     , borderRadius: '0px 16px 16px 0px', outline: 'none',
@@ -16,6 +53,8 @@ function SearchBar({ onSearchClick}) {
                 }}>
                 <img height='24px' src='/search-white-18dp.svg'></img>
             </IconButton>
+        </Box>
+        {data && children({cards: data.cards, onLoadMore})}
         </Box>
     )
 }
