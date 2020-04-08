40728442e5d53162d88041b8518bf97dab208174
francesco <zuppif@usi.ch>
2020-04-08 21:00:50 +0200

    now Stage only hide

--- app/src/Home/NewDeck.js
+++ app/src/Home/NewDeck.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar'
 import { Switch, Route, Link, Redirect, useRouteMatch } from "react-router-dom";
 import Modal from './Modal'
 import { ACTIONS } from '../utils.js'
-import { MagicCard } from './MagicCard'
+import { MagicCard, MagicCardImg } from './MagicCard'
 import queries from '../queries/index.js'
 import Stages from './Stages'
 
@@ -20,6 +20,13 @@ export default function NewDeck({ onClose }) {
     const onDone = (el) => {
         console.log(deck)
     }
+
+    const addCardToDeck = (card) => {
+        const newDeck = Object.assign(deck, { cards: [...deck.cards, card] })
+        setDeck(newDeck)
+    }
+
+    console.log(deck)
 
     return (
         <Card variant='modal'>
@@ -50,21 +57,28 @@ export default function NewDeck({ onClose }) {
                         <Text sx={{ fontSize: 3, fontWeight: 'thin' }}>Cards</Text>
                         <Box py={3} />
                         <SearchBar >{({ cards, onLoadMore }) =>
-                            <Flex  mt={4} sx={{
-                                flexDirection: 'row',
-                                overflowY: 'scroll',
-                                flexWrap: 'wrap',
-                                height: '66vh'
-                            }}>
-                                {cards.cards.map((card) => <Box sx={{ width: ['50%', '33%'] }}>
-                                    <MagicCard p={2} key={card.id} {...card} isZoomable={true} />
-                                </Box>)}
-                                <Flex variant='centering'>
-                                    <Button onClick={onLoadMore}>More</Button>
-                                </Flex>
-                            </Flex>}</SearchBar>
+                            (<div>
+                                {cards.cards.length > 0 &&
+                                    <Flex mt={4} sx={{
+                                        flexDirection: 'row',
+                                        overflowY: 'scroll',
+                                        flexWrap: 'wrap',
+                                        height: '66vh'
+                                    }}>
+                                        {cards.cards.map((card) => <Box key={card.id} p={1} sx={{ width: ['50%', '33%'] }}>
+                                            <MagicCardImg
+                                            {...card} 
+                                            // isZoomable={false} 
+                                            onClick={() => addCardToDeck(card)}/>
+                                        </Box>)}
+                                        <Flex variant='centering'>
+                                            <Button onClick={onLoadMore}>More</Button>
+                                        </Flex>
+                                    </Flex>}
+                            </div>)}</SearchBar>
                         <Box py={2} />
                         <Text pb={2}>So far</Text>
+                        {deck.cards.map((card) => <div>{card.name}</div>)}
                         <Flex py={5}>
                             <Button onClick={onBack}>Back</Button>
                             <Box variant="spacer" />
