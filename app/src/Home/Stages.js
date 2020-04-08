e6e08cce18d39afc8d3e49467e14e46fd8a8cca4
francesco <zuppif@usi.ch>
2020-04-08 21:00:50 +0200

    now Stage only hide

--- app/src/Home/Stages.js
+++ app/src/Home/Stages.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
 import { Button, Flex } from 'theme-ui'
 
+
+const Stage = ({ visible, children }) => (
+
+    <div style={{ transform: visible ? '' : 'scale(0)', height: visible ? 'auto' : '0px' }}>{children}</div>
+)
+
 export default function Stages({ children, initialStage }) {
+    /***
+     * A stage component to "stage" each children component. 
+     * Useful when guiding the user in a process. 
+     * The children are hidden and not delete, thus the state of each component is maintained
+     */
     const [stageKey, setStageKey] = useState(initialStage || 0)
     const onBack = () => setStageKey(Math.max(0, stageKey - 1))
     const onNext = () => setStageKey(Math.min(stageKey + 1, children.length - 1))
 
     const hasBack = stageKey > 0
     const hasNext = stageKey < children.length - 1
+
     return (
         <div>
-            {children[stageKey]({ onBack, onNext, hasBack, hasNext })}
-            {/* {(stageKey > 0) &&
-                    <Button onClick={onBack}>Back</Button>
-                }
-                {(stageKey < children.length - 1) &&
-                    <Button onClick={onNext}>
-                        Next
-                </Button>} */}
+            {children.map((el, i) => <Stage visible={i === stageKey}>
+                {el({ onBack, onNext, hasBack, hasNext })}
+            </Stage>)}
         </div>
     )
 }
