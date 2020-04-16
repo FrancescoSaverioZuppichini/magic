fc52d04da8f090a3c4c6d58a32aad1971de1a58b
francesco <zuppif@usi.ch>
2020-04-16 21:40:40 +0200

    stages for adding magic

--- app/src/Home/MagicCards/MagicCard.js
+++ app/src/Home/MagicCards/MagicCard.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import { Box, Flex, Card, IconButton, Text, Button } from 'theme-ui'
 import Modal from '../Modal.js'
+import Stages from '../Stages'
+import DecksSearchBar from '../Decks/DecksSearchBar'
+import { useQuery } from '@apollo/react-hooks'
+import queries from '../../queries/index.js'
+
 // TODO would be better to go to the next if clicked
 const MagicCardZoom = ({ scryfallId, onClose, active }) => (
     <Modal active={active}>
@@ -30,22 +35,50 @@ const WithControllers = (props) => (
     </Card>
 )
 
-const CardPage = ({ scryfallId, onClose }) => (
-    <Card>
-        <Flex sx={{ flexDirection: 'column' }}>
-            <Flex>
-                <Box variant='spacer' />
-                <Button onClick={onClose}>Close</Button>
-            </Flex>
-            <Box p={2} />
-            <MagicCard scryfallId={scryfallId} />
-            <Box p={2} />
-            <Flex sx={{ justifyContent: 'center' }}>
-                <Button>Add to deck</Button>
-            </Flex>
-        </Flex>
-    </Card>
-)
+const CardPage = ({ scryfallId, onClose }) => {
+
+    const { error, data } = useQuery(queries.GET_ME)
+    return (
+        <Stages initialStage={0}>
+            {({ onNext }) =>
+                <Box variant="vCentering">
+
+                    <Card>
+                        <Flex sx={{ flexDirection: 'column' }}>
+                            <Flex>
+                                <Box variant='spacer' />
+                                <Button onClick={onClose}>Close</Button>
+                            </Flex>
+                            <Box p={2} />
+                            <MagicCard scryfallId={scryfallId} />
+                            <Box p={2} />
+                            <Flex sx={{ justifyContent: 'center' }}>
+                                <Button onClick={onNext}>Add to deck</Button>
+                            </Flex>
+                        </Flex>
+                    </Card>
+                </Box>
+            }
+            {({ onBack, onNext }) =>
+                <Box variant="vCentering">
+                    <Card>
+                        <Text sx={{ fontSize: 2 }}>Add to deck</Text>
+                        <Box py={2} />
+                        <DecksSearchBar decks={data.me.decks} variant="inputTiny">
+                            {decks => decks.map(deck => <Box py={2}>{deck.name}</Box>)
+                            }
+                        </DecksSearchBar>
+                        <Flex sx={{ justifyContent: 'space-between' }}>
+                            <Button onClick={onBack}>Cancel</Button>
+                            <Button onClick={onNext}>Add</Button>
+                        </Flex>
+                    </Card>
+                </Box>
+
+            }
+        </Stages>
+    )
+}
 
 const MagicCard = ({ name, sx, scryfallId, id, upControllers, downControllers, isZoomable = false, variant = 'primary', addable = true }) => {
     const [isZooming, setIsZooming] = useState(false)
