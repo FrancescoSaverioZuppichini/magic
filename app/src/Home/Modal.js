c8189e194c9b0bdb5b72bfa38c850b93aafc5326
francesco <zuppif@usi.ch>
2020-04-16 19:20:56 +0200

    minor changes

--- app/src/Home/Modal.js
+++ app/src/Home/Modal.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Flex, Card } from 'theme-ui'
+import { CSSTransition } from 'react-transition-group'
 
 export default function Modal(props) {
-    return (<div>
-        {props.active && (<Flex p={2} sx={{
+    return (<Box>
+        {props.active && <Flex p={2} sx={{
             flexDirection: 'column',
             position: props.position || 'absolute',
             top: '0', left: 0,
@@ -11,7 +12,8 @@ export default function Modal(props) {
             bg: 'rgba(244, 244, 244, 0.8)',
         }} variant={props.variant || 'centering'}>
             {props.children}
-        </Flex>)}
-    </div>
+        </Flex>
+        }
+    </Box>
     )
 }
