ca28f62b8c00801f09cc5460366b75f7d3addf67
francesco <zuppif@usi.ch>
2020-04-19 11:43:17 +0200

    not working

--- app/src/Home/Decks/Deck.js
+++ app/src/Home/Decks/Deck.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 import { useQuery } from '@apollo/react-hooks'
 import queries from '../../queries'
@@ -7,6 +7,44 @@ import MagicCards from '../MagicCards/MagicCards'
 import { MagicCard, ZoomMagiCardAction, AddToDeckMagiCardAction } from '../MagicCards/MagicCard'
 import DeckControllers from './DeckControllers'
 import { useHistory } from "react-router-dom";
+
+const SelectableMagigCards = (props) => {
+    const [selectedCards, setSelectedCard] = useState([])
+    const containCard = (card) => selectedCards.filter(c => c.id === card.id).length > 0
+
+    const onCardClick = (card) => {
+        let cards = selectedCards
+        if (containCard(card)) {
+            // remove first occurrence
+            for (let i = 0; i < selectedCards.length; i++) {
+                if (selectedCards[i].id === card.id) {
+                    selectedCards.splice(i, 1)
+                    console.log(i)
+                    break
+                }
+            }
+
+            cards = [...selectedCards]
+        }
+        else {
+            cards = [...selectedCards, card]
+        }
+
+        setSelectedCard(cards)
+    }
+
+    return (
+        <Box>
+            <Box>
+                {props.children(selectedCards)}
+            </Box>
+            <MagicCards cards={props.cards}>
+                {(card, i) => props.card(card, i, onCardClick)}
+            </MagicCards>
+        </Box>
+    )
+}
+
 
 export default function Deck({ id }) {
     /**
@@ -27,12 +65,23 @@ export default function Deck({ id }) {
                 <DeckControllers id={data.deck.id} />
                 <Box p={2} />
                 {/* cards */}
-                <MagicCards cards={data.deck.cards}>
+
+                <SelectableMagigCards cards={data.deck.cards} card={
+                    (card, i, setSelectedCard) => <MagicCard key={i} card={card}
+                        onClick={setSelectedCard}
+                        actions={props => <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                            <AddToDeckMagiCardAction {...props} />
+                            <ZoomMagiCardAction {...props} /> </Flex>} />}
+                >
+                    {selectedCards => <Text >{`${selectedCards.length} selected`}</Text>
+                    }
+                </SelectableMagigCards>
+                {/* <MagicCards cards={data.deck.cards}>
                     {(card, i) => <MagicCard key={i} {...card}
                         actions={props => <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
                             <AddToDeckMagiCardAction {...props} />
                             <ZoomMagiCardAction {...props} /> </Flex>} />}
-                </MagicCards>
+                </MagicCards> */}
             </Box>
         }
     </Card>
