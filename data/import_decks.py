9e586f042457c5f5bf0b9532f741c1890f9bc0f1
Francesco <zuppif@usi.ch>
2020-05-05 21:00:55 +0200

    basic functionalities are there

--- data/import_decks.py
+++ data/import_decks.py
@@ -21,23 +21,33 @@ print(f'Importing {len(files)} decks')
 
 
 def convert_deck_cards(deck):
-    converted_deck = {'name': deck['name'], 'cards': [], 'type' : deck['type'], 'releaseDate' : datetime.strptime(deck['releaseDate'], '%Y-%m-%d')}
-    for card in deck['mainBoard']:
-        stored_card = cards.find_one({'scryfallId': card['scryfallId']})
-        if stored_card is None:
-            continue
-        else:
-            [stored_card['_id']]
-            converted_deck['cards'] +=  [ObjectId(stored_card['_id'])] * card['count']
-        
-    converted_deck['default'] = True
-    return converted_deck
+    try:
+        converted_deck = {'name': deck['name'],
+                        'cards': [], 'type': deck['type'],
+                        'releaseDate': datetime.strptime(deck['releaseDate'], '%Y-%m-%d')}
+
+        for card in deck['mainBoard']:
+            stored_card = cards.find_one({'scryfallId': card['scryfallId']})
+            if stored_card is None:
+                continue
+            else:
+                [stored_card['_id']]
+                converted_deck['cards'] += [
+                    ObjectId(stored_card['_id'])] * card['count']
+
+        converted_deck['default'] = True
+        return converted_deck
+    except TypeError:
+        return None
+
 
 def read_convert_store_deck(filepath):
     with open(filepath, 'r') as f:
         deck = json.load(f)
         converted_deck = convert_deck_cards(deck)
-        decks.insert_one(converted_deck)
+        if converted_deck is not None:
+            decks.insert_one(converted_deck)
+
 
 stage = tqdm(th.map(read_convert_store_deck, files, workers=8))
 list(stage)
